f9aa03f2ce5735f79c11cdecd062cb29e65fb16c
isotopeee <johnmichael.doroy@gmail.com>
2018-08-07 23:55:02 +0800

    :sparkles: Added type checking via `prop-types`

--- app/components/Button/Button.js
+++ app/components/Button/Button.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MUIButton from '@material-ui/core/Button';
+
+Button.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+Button.defaultProps = {
+  children: undefined,
+};
 
 function Button(props) {
   return <MUIButton {...props}>{props.children}</MUIButton>;
