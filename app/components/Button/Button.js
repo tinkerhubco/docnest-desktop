428c2f465cc014279cda2f8c4d8b07fb2694624e
isotopeee <johnmichael.doroy@gmail.com>
2018-08-07 23:49:31 +0800

    :rotating_light: :fire: Removed `flow` type checking

--- app/components/Button/Button.js
+++ app/components/Button/Button.js
@@ -1,13 +1,8 @@
-// @flow
 import React from 'react';
 import MUIButton from '@material-ui/core/Button';
 
-import type { Button as MUIButtonProps } from '@material-ui/core';
-
-export type ButtonProps = MUIButtonProps;
-
-export const Button = (props: ButtonProps) => (
-  <MUIButton {...props}>{props.children}</MUIButton>
-);
+function Button(props) {
+  return <MUIButton {...props}>{props.children}</MUIButton>;
+}
 
 export default Button;
