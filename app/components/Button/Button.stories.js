94b8b77af90a14d25b62dcaa376aa4621df6cc7e
isotopeee <johnmichael.doroy@gmail.com>
2018-08-11 23:43:22 +0800

    :sparkles: Added `Button/Contained` and `Button/Text` stories

--- app/components/Button/Button.stories.js
+++ app/components/Button/Button.stories.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import Button from './Button';
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
+const placeholderText = 'Button';
+
+storiesOf('Button/Text', module)
+  .add('default', () => <Button>{placeholderText}</Button>)
+  .add('primary', () => <Button color="primary">{placeholderText}</Button>)
+  .add('secondary', () => <Button color="secondary">{placeholderText}</Button>)
+  .add('disabled', () => <Button disabled>{placeholderText}</Button>);
+
+storiesOf('Button/Contained', module)
+  .add('default', () => <Button variant="contained">{placeholderText}</Button>)
+  .add('primary', () => (
+    <Button variant="contained" color="primary">
+      {placeholderText}
+    </Button>
   ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
+  .add('secondary', () => (
+    <Button variant="contained" color="secondary">
+      {placeholderText}
+    </Button>
+  ))
+  .add('disabled', () => (
+    <Button variant="contained" disabled>
+      {placeholderText}
     </Button>
   ));
