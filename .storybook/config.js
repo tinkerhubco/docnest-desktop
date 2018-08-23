dfc1a262627a26eccb1e398b9fc07e1a7dd8a370
isotopeee <johnmichael.doroy@gmail.com>
2018-08-24 00:50:59 +0800

    :wrench: Configure `babel-plugin-require-context-hook`

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
 import { configure } from '@storybook/react';
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
+
+registerRequireContextHook();
 
 const req = require.context('../app/components', true, /\.stories\.js$/);
 
