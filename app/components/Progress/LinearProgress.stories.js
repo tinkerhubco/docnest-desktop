2c01c75db8448037e497929d2fdf4b2d2aad9d39
jmaicaaan <jmsantosss29@yahoo.com>
2018-08-26 20:26:54 +0800

    Fixed error in <LinearProgress /> story

--- app/components/Progress/LinearProgress.stories.js
+++ app/components/Progress/LinearProgress.stories.js
@@ -4,6 +4,6 @@ import { storiesOf } from '@storybook/react';
 import LinearProgress from './LinearProgress';
 
 storiesOf('LinearProgress', module)
-  // .add('default', <LinearProgress />) throwing error for some reason
+  .add('default', () => <LinearProgress />)
   .add('primary', () => <LinearProgress color="primary" />)
   .add('secondary', () => <LinearProgress color="secondary" />);
