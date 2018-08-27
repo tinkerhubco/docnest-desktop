6b274b3bfa276a77219887f05be28691c58ecf24
isotopeee <johnmichael.doroy@gmail.com>
2018-08-27 19:23:03 +0800

    :recycle: Use wrapped `MenuItem`

--- app/components/FormField/Select.js
+++ app/components/FormField/Select.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
+
+import MenuItem from '../Menu/MenuItem';
 
 SelectFormField.propTypes = {
   id: PropTypes.string.isRequired,
