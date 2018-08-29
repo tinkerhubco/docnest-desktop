647fdc4d7a5de24118aa10e978e2ed8945d628d8
isotopeee <johnmichael.doroy@gmail.com>
2018-08-29 23:55:48 +0800

    :recycle: Add `Checkbox` propTypes and update stories

--- app/components/Checbox/Checkbox.js
+++ app/components/Checbox/Checkbox.js
@@ -5,15 +5,71 @@ import styled from 'styled-components';
 import MUICheckbox from '@material-ui/core/Checkbox';
 
 Checkbox.propTypes = {
-  checked: PropTypes.bool,
-  color: PropTypes.oneOf(['primary', 'secondary']),
-  disabled: PropTypes.bool
+  /**
+   * If `true`, the component is checked.
+   */
+  checked: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  /**
+   * The icon to display when the component is checked.
+   */
+  checkedIcon: PropTypes.node,
+  /**
+   * The color of the component. It supports those theme colors that make sense for this component.
+   */
+  color: PropTypes.oneOf(['primary', 'secondary', 'default']),
+  /**
+   * If `true`, the switch will be disabled.
+   */
+  disabled: PropTypes.bool,
+  /**
+   * If `true`, the ripple effect will be disabled.
+   */
+  disableRipple: PropTypes.bool,
+  /**
+   * The icon to display when the component is unchecked.
+   */
+  icon: PropTypes.node,
+  /**
+   * The id of the `input` element.
+   */
+  id: PropTypes.string,
+  /**
+   * If `true`, the component appears indeterminate.
+   */
+  indeterminate: PropTypes.bool,
+  /**
+   * The icon to display when the component is indeterminate.
+   */
+  indeterminateIcon: PropTypes.node,
+  /**
+   * Properties applied to the `input` element.
+   */
+  inputProps: PropTypes.object,
+  /**
+   * Use that property to pass a ref callback to the native input component.
+   */
+  inputRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+  /**
+   * Callback fired when the state is changed.
+   *
+   * @param {object} event The event source of the callback.
+   * You can pull out the new value by accessing `event.target.checked`.
+   * @param {boolean} checked The `checked` value of the switch
+   */
+  onChange: PropTypes.func,
+  /**
+   * The input component property `type`.
+   */
+  type: PropTypes.string,
+  /**
+   * The value of the component.
+   */
+  value: PropTypes.string
 };
 
 Checkbox.defaultProps = {
-  checked: false,
   color: 'primary',
-  disabled: false
+  indeterminate: false
 };
 
 function Checkbox(props) {
--- app/components/Checbox/Checkbox.stories.js
+++ app/components/Checbox/Checkbox.stories.js
@@ -8,9 +8,3 @@ storiesOf('Checkbox', module)
   .add('primary', () => <Checkbox color="primary" />)
   .add('secondary', () => <Checkbox color="secondary" />)
   .add('disabled', () => <Checkbox disabled />);
-
-storiesOf('Checkbox/Checked', module)
-  .add('default', () => <Checkbox checked />)
-  .add('primary', () => <Checkbox checked color="primary" />)
-  .add('secondary', () => <Checkbox checked color="secondary" />)
-  .add('disabled', () => <Checkbox checked disabled />);
