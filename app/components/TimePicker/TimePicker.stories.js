60f8d28961bf170086c564dfa89ee5334da65d62
isotopeee <johnmichael.doroy@gmail.com>
2018-09-01 03:35:22 +0800

    :white_check_mark: :recycle: Make stories and snapshot tests deterministic

--- app/components/TimePicker/TimePicker.stories.js
+++ app/components/TimePicker/TimePicker.stories.js
@@ -3,11 +3,46 @@ import { storiesOf } from '@storybook/react';
 
 import TimePicker from './TimePicker';
 
-const currentDate = new Date();
+const placeholderDateDefault = new Date('2018-09-05T00:00:00Z');
 
 storiesOf('TimePicker', module)
-  .add('default', () => <TimePicker label="Default" value={currentDate} onChange={() => {}} />)
-  .add('autoOk', () => <TimePicker label="Auto Ok" autoOk value={currentDate} onChange={() => {}} />)
-  .add('clearable', () => <TimePicker label="Clearable" clearable value={currentDate} onChange={() => {}} />)
-  .add('Turn off AM PM', () => <TimePicker label="AM PM" ampm={false} value={currentDate} onChange={() => {}} />)
-  .add('showTodayButton', () => <TimePicker label="Show Today Button" showTodayButton todayLabel="Now" value={currentDate} onChange={() => {}} />)
+  .add('default', () => (
+    <TimePicker
+      label="Default"
+      value={placeholderDateDefault}
+      onChange={() => {}}
+    />
+  ))
+  .add('autoOk', () => (
+    <TimePicker
+      label="Auto Ok"
+      autoOk
+      value={placeholderDateDefault}
+      onChange={() => {}}
+    />
+  ))
+  .add('clearable', () => (
+    <TimePicker
+      label="Clearable"
+      clearable
+      value={placeholderDateDefault}
+      onChange={() => {}}
+    />
+  ))
+  .add('Turn off AM PM', () => (
+    <TimePicker
+      label="AM PM"
+      ampm={false}
+      value={placeholderDateDefault}
+      onChange={() => {}}
+    />
+  ))
+  .add('showTodayButton', () => (
+    <TimePicker
+      label="Show Today Button"
+      showTodayButton
+      todayLabel="Now"
+      value={placeholderDateDefault}
+      onChange={() => {}}
+    />
+  ));
