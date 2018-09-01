90509158f97ccd8b5038138aa91a0f4bad33ca86
isotopeee <johnmichael.doroy@gmail.com>
2018-09-01 19:58:21 +0800

    :wrench: Disable `storyshots` for components that can't be open without interacting with DOM (injected in body)

--- test/storyshots/storyshots.spec.js
+++ test/storyshots/storyshots.spec.js
@@ -1,4 +1,10 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import 'jest-styled-components';
 
-initStoryshots({});
+// TODO: Find a way to snapshot components injected to the DOM
+initStoryshots({
+  // Disable storyshots for components that can't be open
+  // without interacting with DOM (they are injected in body) (e.g. modal)
+  // https://github.com/mui-org/material-ui/issues/10679
+  storyKindRegex: /^((?!.*?(Snackbar|Menu(?![A-Za-z]))).)*$/
+});
