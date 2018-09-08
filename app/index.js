147f79d492299dd70e26a89bcab5b819fb35d4a0
isotopeee <johnmichael.doroy@gmail.com>
2018-09-08 15:40:46 +0800

    :ok_hand: :truck: Move `App.js` and `Root.js` to `app` directory

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import Root from './screens/Root';
+import Root from './Root';
 import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 
@@ -15,8 +15,8 @@ render(
 );
 
 if (module.hot) {
-  module.hot.accept('./screens/Root', () => {
-    const NextRoot = require('./screens/Root'); // eslint-disable-line global-require
+  module.hot.accept('./Root', () => {
+    const NextRoot = require(`./Root`); // eslint-disable-line global-require
     render(
       <AppContainer>
         <NextRoot store={store} history={history} />
--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,8 @@
 /* eslint flowtype-errors/show-errors: 0 */
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import App from './screens/App';
+
+import App from './App';
 import Login from './screens/Login';
 
 export default () => (
