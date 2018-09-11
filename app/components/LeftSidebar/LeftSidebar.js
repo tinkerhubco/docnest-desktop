f39f4c5f5bd6d4e34c01f07536222f0e4bfd4e33
isotopeee <johnmichael.doroy@gmail.com>
2018-09-12 00:19:32 +0800

    :art: Enhance `LeftSidebar` styles

--- app/components/LeftSidebar/LeftSidebar.js
+++ app/components/LeftSidebar/LeftSidebar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Drawer from '@material-ui/core/Drawer';
+import { withTheme } from '@material-ui/core/styles';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 import Divider from '../Divider/Divider';
@@ -24,33 +25,63 @@ const StyledRootDiv = styled.div`
 `;
 
 const StyledDrawer = styled(Drawer)`
-  position: relative;
-  white-space: nowrap;
-  width: ${drawerWidth}px;
-  ${props => (props.open ? '' : 'overflow-x: hidden')};
+  > div {
+    position: relative;
+    white-space: nowrap;
+    width: ${drawerWidth}px;
+    ${props =>
+      props.open
+        ? `transition: ${props.theme.transitions.create('width', {
+            easing: props.theme.transitions.easing.sharp,
+            duration: props.theme.transitions.duration.enteringScreen
+          })};`
+        : `transition: ${props.theme.transitions.create('width', {
+            easing: props.theme.transitions.easing.sharp,
+            duration: props.theme.transitions.duration.leavingScreen
+          })};
+        overflow-x: hidden;
+        width: ${props.theme.spacing.unit * 7}px;
+        ${props.theme.breakpoints.up('sm')} {
+          width: ${props.theme.spacing.unit * 9}px;
+        }
+  `};
+  }
 `;
+
+const StyledDrawerWithTheme = withTheme()(StyledDrawer);
 
 const StyledToolbarDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
   padding: 0 8px;
+  @media (min-width: 0px) and (orientation: landscape) {
+    min-height: 48px;
+  }
+  @media (min-width: 600px) {
+    min-height: 64px;
+  }
+  min-height: 56px;
 `;
+
+const StyledToolbarDivWithTheme = withTheme()(StyledToolbarDiv);
 
 export function LeftSidebar(props) {
   const { menuItems, open, renderMenuItems, onCollapseClick } = props;
 
   return (
     <StyledRootDiv>
-      <StyledDrawer variant="permanent" open={open}>
-        <StyledToolbarDiv>
-          <IconButton onClick={event => onCollapseClick(event, open)}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </StyledToolbarDiv>
+      <StyledDrawerWithTheme variant="permanent" open={open}>
+        <StyledToolbarDivWithTheme>
+          {open && (
+            <IconButton onClick={event => onCollapseClick(event, open)}>
+              <ChevronLeftIcon />
+            </IconButton>
+          )}
+        </StyledToolbarDivWithTheme>
         <Divider />
         <List>{renderMenuItems(menuItems)}</List>
-      </StyledDrawer>
+      </StyledDrawerWithTheme>
     </StyledRootDiv>
   );
 }
@@ -65,9 +96,9 @@ LeftSidebar.propTypes = {
 LeftSidebar.defaultProps = {
   renderMenuItems: menuItems =>
     menuItems.map(({ title, icon }) => (
-      <ListItem button>
+      <ListItem key={title} button>
         {icon && <ListItemIcon>{icon}</ListItemIcon>}
-        <ListItemText key={title} primary={title} />
+        <ListItemText primary={title} />
       </ListItem>
     ))
 };
