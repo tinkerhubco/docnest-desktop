9fe92504d4d75f426059b8880c2ba2047204cb68
isotopeee <johnmichael.doroy@gmail.com>
2018-09-15 19:57:43 +0800

    :sparkles: Create `DefaultLayout` layout component

---
+++ app/layout/DefaultLayout.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import { ROUTES } from '../Routes';
+import Header from '../components/Header/Header';
+import LeftSidebar from '../components/LeftSidebar/LeftSidebar';
+import ListItemLink from '../components/List/ListItemLink';
+
+const StyledRootDiv = styled.div`
+  position: relative;
+  display: flex;
+  flex-grow: 1;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const StyledMain = styled.main`
+  @media (min-width: 0px) and (orientation: landscape) {
+    margin-top: 48px;
+  }
+  @media (min-width: 600px) {
+    margin-top: 64px;
+  }
+  flex: 1;
+  margin-top: 56px;
+`;
+
+export class DefaultLayout extends React.Component {
+  state = {
+    openSidebar: false
+  };
+
+  handleLeftMenuIconClicked = () =>
+    this.setState({
+      openSidebar: true
+    });
+
+  handleCollapseClick = () =>
+    this.setState({
+      openSidebar: false
+    });
+
+  render() {
+    const { openSidebar } = this.state;
+
+    return (
+      <StyledRootDiv>
+        <Header
+          title="DocNest"
+          onLeftMenuIconClicked={this.handleLeftMenuIconClicked}
+          open={openSidebar}
+        />
+        <LeftSidebar
+          menuItems={ROUTES}
+          renderMenuItems={menuItems =>
+            menuItems.map(({ icon, title, path }) => (
+              <ListItemLink key={path} path={path} title={title} icon={icon} />
+            ))
+          }
+          onCollapseClick={this.handleCollapseClick}
+          open={openSidebar}
+        />
+        <StyledMain>{this.props.children}</StyledMain>
+      </StyledRootDiv>
+    );
+  }
+}
+
+DefaultLayout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default DefaultLayout;
