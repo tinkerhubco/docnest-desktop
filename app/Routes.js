48f5b089d9ae63051b4a94d4681103d33f7e708e
isotopeee <johnmichael.doroy@gmail.com>
2018-09-15 19:48:03 +0800

    :sparkles: Centralize routes

--- app/Routes.js
+++ app/Routes.js
@@ -2,12 +2,19 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import FolderIcon from '@material-ui/icons/Folder';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+
 import App from './App';
-
 import DefaultLayout from './layout/DefaultLayout';
-
 import Login from './screens/Login';
 import AppointmentSchedule from './screens/AppointmentSchedule';
+import PatientList from './screens/PatientList';
 
 function ProtectedRoute({ component: Component, ...rest }) {
   // This will supplied by `AuthService`
@@ -38,12 +45,65 @@ function ProtectedRoute({ component: Component, ...rest }) {
   );
 }
 
+export const ROUTES = [
+  {
+    path: '/',
+    exact: true,
+    title: 'Dashboard',
+    component: () => <div>Dashboard</div>,
+    icon: <DashboardIcon />
+  },
+  {
+    path: '/calendar',
+    title: 'Calendar',
+    component: () => <div>Calendar</div>,
+    icon: <CalendarTodayIcon />
+  },
+  {
+    path: '/documents',
+    title: 'Documents',
+    component: () => <div>Documents</div>,
+    icon: <FolderIcon />
+  },
+  {
+    path: '/insurance',
+    title: 'Insurance',
+    component: () => <div>Insurance</div>,
+    icon: <AccountBalanceIcon />
+  },
+  {
+    path: '/pharmacies',
+    title: 'Pharmacies',
+    component: () => <div>Pharmacies</div>,
+    icon: <LocalHospitalIcon />
+  },
+  {
+    path: '/patients',
+    title: 'Patient Records',
+    component: PatientList,
+    icon: <AssignmentIndIcon />
+  },
+  {
+    path: '/appointments',
+    title: 'Appointments',
+    component: AppointmentSchedule,
+    icon: <ScheduleIcon />
+  }
+];
+
 export function Routes() {
   return (
     <App>
       <Switch>
         <Route path="/login" component={Login} />
-        <ProtectedRoute path="/" component={AppointmentSchedule} />
+        {ROUTES.map(({ path, component, exact }) => (
+          <ProtectedRoute
+            key="path"
+            path={path}
+            component={component}
+            exact={exact}
+          />
+        ))}
       </Switch>
     </App>
   );
