2936bf6170a523fc91eb83627d45037bc1e340f3
isotopeee <johnmichael.doroy@gmail.com>
2018-09-20 01:16:15 +0800

    :construction: WIP - Dynamic 'PatientMedicalForm'

--- app/screens/patientInformation/PatientMedicalForm.js
+++ app/screens/patientInformation/PatientMedicalForm.js
@@ -2,72 +2,91 @@ import React from 'react';
 import { Field, Formik } from 'formik';
 import styled from 'styled-components';
 
-import Form from '../../components/Form/Form';
-import TextField from '../../components/FormField/TextField';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+
 import DatePicker from '../../components/DatePicker/DatePicker';
-import Button from '../../components/Button/Button';
+import Form from '../../components/Form/Form';
+import Grid from '../../components/Grid/Grid';
+import InputAdornment from '../../components/Input/InputAdornment';
+import TextField from '../../components/FormField/TextField';
 import Typography from '../../components/Typography/Typography';
 
-const StyledContainer = styled.div``;
-const StyledFormContainer = styled.div``;
-const StyledForm = styled(Form)``;
+const StyledDatePicker = styled(DatePicker)`
+  > div {
+    align-items: unset;
+  }
+`;
 
 export function PatientMedicalForm() {
   return (
-    <StyledContainer>
+    <React.Fragment>
       <Typography variant="title">Medical History</Typography>
-      <StyledFormContainer>
-        <Formik
-          initialValues={{
-            email: '',
-            password: ''
-          }}
-          onSubmit={values => {
-            console.log(values);
-          }}
-          render={() => (
-            <StyledForm>
-              <Field
-                name="healthCondition"
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    id="healthCondition"
-                    label="Health Condition"
-                    margin="normal"
-                    fullWidth
-                  />
-                )}
-              />
-              <Field
-                name="treatment"
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    id="treatment"
-                    label="Treatment"
-                    margin="normal"
-                    fullWidth
-                  />
-                )}
-              />
-              <Field
-                name="date"
-                render={({ field }) => <DatePicker {...field} autoOk />}
-              />
-              <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                fullWidth
-              >
-                Add Patient Medical
-              </Button>
-            </StyledForm>
-          )}
-        />
-      </StyledFormContainer>
-    </StyledContainer>
+      <Formik
+        initialValues={{
+          email: '',
+          password: ''
+        }}
+        onSubmit={values => {
+          console.log(values);
+        }}
+        render={({ setFieldValue }) => (
+          <Form>
+            <Grid container spacing={24}>
+              <Grid item>
+                <Field
+                  name="healthCondition"
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      id="healthCondition"
+                      label="Health Condition"
+                      margin="normal"
+                      fullWidth
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item>
+                <Field
+                  name="treatment"
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      id="treatment"
+                      label="Treatment"
+                      margin="normal"
+                      fullWidth
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item>
+                <Field
+                  name="date"
+                  render={({ field }) => (
+                    <StyledDatePicker
+                      {...field}
+                      autoOk
+                      id="birthdate"
+                      label="Date of Birth"
+                      margin="normal"
+                      onChange={date => setFieldValue('date', date)}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <DateRangeIcon />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      />
+    </React.Fragment>
   );
 }
 
