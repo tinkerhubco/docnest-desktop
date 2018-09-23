305eadf2bbc90233cee281a9e729f8f2c506259b
isotopeee <johnmichael.doroy@gmail.com>
2018-09-23 16:20:32 +0800

    :recycle: Refactor 'PatientMedicalForm' UI

--- app/screens/patientInformation/PatientMedicalForm.js
+++ app/screens/patientInformation/PatientMedicalForm.js
@@ -1,3 +1,7 @@
+/**
+ * TODO: Stand alone form with formik
+ */
+
 import React from 'react';
 import { Field, Formik } from 'formik';
 import styled from 'styled-components';
@@ -7,6 +11,7 @@ import DateRangeIcon from '@material-ui/icons/DateRange';
 import DatePicker from '../../components/DatePicker/DatePicker';
 import Form from '../../components/Form/Form';
 import Grid from '../../components/Grid/Grid';
+import InputChip from '../../components/InputChip/InputChip';
 import InputAdornment from '../../components/Input/InputAdornment';
 import TextField from '../../components/FormField/TextField';
 import Typography from '../../components/Typography/Typography';
@@ -17,60 +22,143 @@ const StyledDatePicker = styled(DatePicker)`
   }
 `;
 
+const StyledInputChip = styled(InputChip)`
+  && > div {
+    margin-top: 20px;
+  }
+`;
+
 export function PatientMedicalForm() {
   return (
     <React.Fragment>
       <Typography variant="title">Medical History</Typography>
       <Formik
         initialValues={{
-          email: '',
-          password: ''
+          healthConditions: [],
+          medications: [],
+          treatments: [],
+          diagnostic: '',
+          diagnosticDate: new Date()
         }}
         onSubmit={values => {
           console.log(values);
         }}
-        render={({ setFieldValue }) => (
+        render={({
+          values: { healthConditions, medications, treatments },
+          setFieldValue
+        }) => (
           <Form>
+            <Grid container>
+              <Grid item xs={12}>
+                <Field
+                  name="healthConditions"
+                  render={({ field }) => (
+                    <StyledInputChip
+                      {...field}
+                      fullWidth
+                      id="healthCondition"
+                      label="Health Conditions"
+                      margin="normal"
+                      onAdd={healthCondition => {
+                        const newHealthConditions = [
+                          ...healthConditions,
+                          healthCondition
+                        ];
+
+                        setFieldValue(field.name, newHealthConditions);
+                      }}
+                      onDelete={(healthCondition, index) => {
+                        const newHealthConditions = [
+                          ...healthConditions.slice(0, index),
+                          ...healthConditions.slice(index + 1)
+                        ];
+
+                        setFieldValue(field.name, newHealthConditions);
+                      }}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Field
+                  name="medications"
+                  render={({ field }) => (
+                    <StyledInputChip
+                      {...field}
+                      fullWidth
+                      id="medications"
+                      label="Medications"
+                      margin="normal"
+                      onAdd={medication => {
+                        const newMedications = [...medications, medication];
+
+                        setFieldValue(field.name, newMedications);
+                      }}
+                      onDelete={(medication, index) => {
+                        const newMedications = [
+                          ...medications.slice(0, index),
+                          ...medications.slice(index + 1)
+                        ];
+
+                        setFieldValue(field.name, newMedications);
+                      }}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Field
+                  name="treatments"
+                  render={({ field }) => (
+                    <StyledInputChip
+                      {...field}
+                      fullWidth
+                      id="treatments"
+                      label="Treatments"
+                      margin="normal"
+                      onAdd={treatment => {
+                        const newTreatments = [...treatments, treatment];
+
+                        setFieldValue(field.name, newTreatments);
+                      }}
+                      onDelete={(treatment, index) => {
+                        const newTreatments = [
+                          ...treatments.slice(0, index),
+                          ...treatments.slice(index + 1)
+                        ];
+
+                        setFieldValue(field.name, newTreatments);
+                      }}
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
             <Grid container spacing={24}>
               <Grid item>
                 <Field
-                  name="healthCondition"
+                  name="diagnostic"
                   render={({ field }) => (
                     <TextField
                       {...field}
-                      id="healthCondition"
-                      label="Health Condition"
+                      id="diagnostic"
+                      label="Diagnostic"
                       margin="normal"
-                      fullWidth
                     />
                   )}
                 />
               </Grid>
               <Grid item>
                 <Field
-                  name="treatment"
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      id="treatment"
-                      label="Treatment"
-                      margin="normal"
-                      fullWidth
-                    />
-                  )}
-                />
-              </Grid>
-              <Grid item>
-                <Field
-                  name="date"
+                  name="diagnosticDate"
                   render={({ field }) => (
                     <StyledDatePicker
                       {...field}
                       autoOk
-                      id="birthdate"
-                      label="Date of Birth"
+                      id="diagnosticDate"
+                      label="Diagnostic Date"
                       margin="normal"
-                      onChange={date => setFieldValue('date', date)}
+                      onChange={date => setFieldValue(field.name, date)}
                       InputProps={{
                         startAdornment: (
                           <InputAdornment position="start">
