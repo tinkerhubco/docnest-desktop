12de75c8e341e9a3dd3e9363c8e764b9e68dfc31
isotopeee <johnmichael.doroy@gmail.com>
2018-10-01 00:49:57 +0800

    GH-29: :building_construction: :truck: :recycle: Rename subforms and refactor implementation

--- app/screens/patientInformation/PatientMedicalForm.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import PatientMedicalForm from './PatientMedicalForm';
-
-storiesOf('Screens/PatientInformation/PatientMedicalForm', module).add(
-  'default',
-  () => <PatientMedicalForm />
-);
