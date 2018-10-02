cf0efc7e3c07051e7caaa78a411e0ce9cfd489b2
isotopeee <johnmichael.doroy@gmail.com>
2018-10-02 20:52:04 +0800

    GH-29: :sparkles: Compose `PatientDetailsConnector` and use conversions module

--- app/screens/PatientDetails.js
+++ app/screens/PatientDetails.js
@@ -2,49 +2,37 @@ import React from 'react';
 
 import SaveIcon from '@material-ui/icons/Save';
 
+import PatientDetailsConnector from './patientDetails/PatientDetailsConnector';
 import PatientDetailsForm from './patientDetails/PatientDetailsForm';
 import MainContent from '../components/MainContent/MainContent';
 
-// TODO: Will be supplied by 'PatientDetailsForm'
-// with the help of connector or/and conversions module
-
-const initialValues = {
-  healthConditions: [],
-  medications: [],
-  treatments: [],
-  diagnostic: '',
-  diagnosticDate: new Date(),
-  firstName: '',
-  middleName: '',
-  lastName: '',
-  birthDate: new Date(),
-  gender: '',
-  weight: '',
-  height: '',
-  healthCard: '',
-  emergencyContact: '',
-  address: '',
-  phone: '',
-  bloodType: ''
-};
+import { patientDetailsConversions as conversions } from '../utils/conversions';
 
 export class PatientDetails extends React.Component {
   render() {
     return (
-      <PatientDetailsForm
-        initialValues={initialValues}
-        onSubmit={values => console.log(values)}
-      >
-        {({ form, formik: { handleSubmit } }) => (
-          <MainContent
-            title="Patient Information"
-            buttonIcon={<SaveIcon />}
-            onActionClick={handleSubmit}
+      <PatientDetailsConnector>
+        {({ patientFetcher, patientUpdater }) => (
+          <PatientDetailsForm
+            initialValues={conversions.responseToFormValues(
+              patientFetcher.patient
+            )}
+            onSubmit={values =>
+              patientUpdater.execute(conversions.formValuesToRequest(values))
+            }
           >
-            {form}
-          </MainContent>
+            {({ form, formik: { handleSubmit } }) => (
+              <MainContent
+                title="Patient Information"
+                buttonIcon={<SaveIcon />}
+                onActionClick={handleSubmit}
+              >
+                {form}
+              </MainContent>
+            )}
+          </PatientDetailsForm>
         )}
-      </PatientDetailsForm>
+      </PatientDetailsConnector>
     );
   }
 }
