6b41ff94fbf1f974a7beaeade6c3c6e123941f03
isotopeee <johnmichael.doroy@gmail.com>
2018-10-02 20:46:55 +0800

    GH-29: :sparkles: Create conversions utility module

---
+++ app/utils/conversions.js
@@ -0,0 +1,14 @@
+export const patientDetailsConversions = {
+  responseToFormValues: response => {
+    // conversions here
+    return response;
+  },
+  formValuesToRequest: values => {
+    // conversions here
+    return values;
+  }
+};
+
+export default {
+  patientDetailsConversions
+};
