7a6f86a4210350971779e2c96f84a75d3ddde6c3
isotopeee <johnmichael.doroy@gmail.com>
2018-10-13 16:24:45 +0800

    GH-29: :building_construction: :truck: Rename `AppointmentScheduleSearch` to `AppointmentScheduleSearchForm` and refactor implementation

--- app/screens/appointmentSchedule/AppointmentScheduleSearch.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import AppointmentScheduleSearch from './AppointmentScheduleSearch';
-
-storiesOf('Screens/AppointmentSchedule/AppointmentScheduleSearch', module).add(
-  'default',
-  () => <AppointmentScheduleSearch />
-);
