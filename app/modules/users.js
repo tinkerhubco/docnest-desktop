be29661a275b69cceae52a7484adaa1c9e0d2fe8
isotopeee <johnmichael.doroy@gmail.com>
2018-10-16 07:18:40 +0800

    GH-26: :recycle: Adjust 'users' reducer

--- app/modules/users.js
+++ app/modules/users.js
@@ -2,9 +2,17 @@ export const STATE_KEY = 'users';
 
 export const ADD_USERS = 'app.users.ADD_USERS';
 
+export const ADD_USER_APPOINTMENT =
+  'app.user.appointments.ADD_USER_APPOINTMENT';
+
 export const addUsers = entities => ({
   type: ADD_USERS,
   payload: entities
+});
+
+export const addUserAppointment = ({ userId, appointmentId }) => ({
+  type: ADD_USER_APPOINTMENT,
+  payload: { userId, appointmentId }
 });
 
 export default function users(state = {}, { payload, type }) {
@@ -13,6 +21,18 @@ export default function users(state = {}, { payload, type }) {
       return {
         ...state,
         ...payload[STATE_KEY]
+      };
+
+    case ADD_USER_APPOINTMENT:
+      return {
+        ...state,
+        [payload.userId]: {
+          ...state[payload.userId],
+          appointments: [
+            ...state[payload.userId].appointments,
+            payload.appointmentId
+          ]
+        }
       };
 
     default:
