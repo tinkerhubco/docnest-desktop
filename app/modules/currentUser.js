4cee747d314c068f6838acbbebd7b12fcef5ed4d
isotopeee <johnmichael.doroy@gmail.com>
2018-10-16 07:16:55 +0800

    GH-26: :recycle: Adjust 'currentUser' reducer

--- app/modules/currentUser.js
+++ app/modules/currentUser.js
@@ -1,35 +1,18 @@
 export const STATE_KEY = 'currentUser';
 
-export const ADD_CURRENT_USER = 'app.currentUser.ADD_CURRENT_USER';
-export const ADD_CURRENT_USER_APPOINTMENT =
-  'app.currentUser.appointments.ADD_CURRENT_USER_APPOINTMENTS';
+export const SET_CURRENT_USER = 'app.currentUser.SET_CURRENT_USER';
 
-export const addCurrentUser = entities => ({
-  type: ADD_CURRENT_USER,
-  payload: entities
+export const setCurrentUser = userId => ({
+  type: SET_CURRENT_USER,
+  payload: {
+    userId
+  }
 });
 
-export const addCurrentUserAppointment = ({ appointmentId }) => ({
-  type: ADD_CURRENT_USER_APPOINTMENT,
-  payload: { appointmentId }
-});
-
-export default function currentUser(state = {}, { payload, type }) {
+export default function currentUser(state = '', { payload, type }) {
   switch (type) {
-    case ADD_CURRENT_USER:
-      // Remove other properties except id and appointments
-      return {
-        ...state,
-        ...Object.entries(payload[STATE_KEY]).map(([id, data]) => ({
-          id,
-          appointments: data.appointments
-        }))[0]
-      };
-    case ADD_CURRENT_USER_APPOINTMENT:
-      return {
-        ...state,
-        appointments: [...state.appointments, payload.appointmentId]
-      };
+    case SET_CURRENT_USER:
+      return payload.userId;
 
     default:
       return state;
